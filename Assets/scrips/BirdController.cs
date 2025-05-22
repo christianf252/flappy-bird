2b22fdc30caab94a632391e81e44db30c047a4cc
christianf252 <christianf252@nycstudents.net>
2025-05-22 08:55:34 -0400

    Completed scoring

--- Assets/scrips/BirdController.cs
+++ Assets/scrips/BirdController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class BirdController : MonoBehaviour
 {
+    int score = 0;
+    public Text scoreUI;
     Rigidbody2D bird;
     // Start is called before the first frame update
     void Start()
@@ -23,4 +25,16 @@ public class BirdController : MonoBehaviour
     
         
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Points"))
+        {
+            // Increase score by 1
+            score = score + 1;
+
+            scoreUI.text = score.ToString();
+        }
+    }
+    
 }
